44b06a8b2900ae1724fc8142dd68aa40f496f375
My Mac <mymac@Chance-MacBook.localdomain>
2017-09-12 16:52:21 -0500

    better labeled files

--- py-exercises/week1day2-list.py
+++ py-exercises/week1day2-list.py
@@ -1,50 +1,56 @@
 #Day 2 exercises - python lists and tuples
-
+grades = [7, 6, 5, -7, -6, -5]
+print (grades)
 def sumnum(n):
     count = 0
     for i in n:
         count = count + i
-    return count
-
-grades = [7, 6, 5, -7, -6, -5]
-#print(sumnum(grades))
+    print(count)
+sumnum(grades)
 
 #print the largest of the numbers
 def largest(n):
     print (max(n))
+largest(grades)
 
 def smallest(n):
     print (min(n))
+smallest(grades)
 
 def evennum(n):
     for i in n:
         if i % 2 == 0:
             print (i)
+evennum(grades)
 
 def positivenum(n):
     for i in n:
         if i > 0:
             print (i)
+positivenum(grades)
 
 def positivenumlist(n):
     posnum = []
     for i in n:
         if i > 0:
             posnum.append(i)
-    print posnum
+    print (posnum)
+positivenumlist(grades)
 
 def multiplelist(n):
     factor = 3
     newlist = []
-    for i in grades:
+    for i in n:
         newlist.append(factor * i)
     print (newlist)
+multiplelist(grades)
 
 #multiplication vectors
 def multiplicatiovectors(n):
     anotherlist = [1,2,3,4,5,6]
     for i in range(0,len(n)):
         print (n[i] * anotherlist[i])
+multiplicatiovectors(grades)
 
 #additionmatrix that works for any pair of matrices of any size, as long as they have the same size
 def additionmatrix(x,y):
@@ -53,6 +59,7 @@ def additionmatrix(x,y):
             print(matrix1[i][j] + matrix2[i][j])
 matrix1 = [[2,-2],[5,3]]
 matrix2 = [[6,5], [4,3]]
+additionmatrix(matrix1, matrix2)
 
 #de-dup, remove duplicates from a lists
 def dedup(n):
@@ -62,6 +69,7 @@ def dedup(n):
             unique.append(i)
     print (unique)
 numbers = [1,2,3,3,2,2,1,5]
+dedup(numbers)
 
 #bonus: matrix multiplication
 for i in range(len(matrix1)):
