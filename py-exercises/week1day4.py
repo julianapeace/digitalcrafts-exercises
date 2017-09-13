c8b7155b47d3aed031f11a5ef38eba9aef6086b6
My Mac <mymac@Chance-MacBook.localdomain>
2017-09-13 09:00:15 -0500

    update

--- py-exercises/week1day4.py
+++ py-exercises/week1day4.py
@@ -1,2 +1,9 @@
-for x in range(-3, 5):
-  print("f({x})={y} \t g({x})={z}".format(x=x, y=f(x), z=g(x)))
+from matplotlib import pyplot
+f_output = []
+g_output = []
+x_list = list(range(-3, 5))
+for x in x_list:
+  f_output.append(f(x))
+  g_output.append(g(x))
+pyplot.plot(x_list, f_output, x_list, g_output)
+pyplot.show()
