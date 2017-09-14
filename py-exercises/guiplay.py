69c73c0d970cc864dad8b1cb0d41160327752df2
My Mac <mymac@Chance-MacBook.localdomain>
2017-09-14 08:56:52 -0500

    update

--- py-exercises/guiplay.py
+++ py-exercises/guiplay.py
@@ -4,3 +4,4 @@ from tkinter import ttk # To use the stylized, "look and feel" widgets
 application_window = tk.Tk()
 
 cmd_button = tk.Button(application_window, text = "Example")
+cmd_button.grid(row=3, column=1, sticky=tk.E + tk.W, pady=10)
