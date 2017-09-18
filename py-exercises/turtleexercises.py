888bfe701eaf0967059ab17eceff02458d1f59fc
My Mac <chancecordelia@gmail.com>
2017-09-17 22:42:46 -0500

    weekend play

--- py-exercises/turtleexercises.py
+++ py-exercises/turtleexercises.py
@@ -1,6 +1,8 @@
 from turtle import *
 import random
 speed("fastest")
+hideturtle()
+title('Night Sky')
 
 def drawstar(x):
     color('white')
@@ -12,6 +14,7 @@ def drawstar(x):
 
 def nightsky():
     bgcolor('#000080')
+    bgpic("1.jpg")
     for i in range(20):
         x = random.randint(-300,300)
         y = random.randint(-300,300)
@@ -22,4 +25,28 @@ def nightsky():
         drawstar(size)
 # nightsky()
 
+def diamond(x):
+    color('white')
+    pencolor('gray')
+    begin_fill()
+    seth(45)
+    for i in range(4):
+        forward(x)
+        right(90)
+    end_fill()
+# diamond(50)
+
+def nightsky2():
+    bgcolor('black')
+    for i in range(40):
+        x = random.randint(-300,300)
+        y = random.randint(-300,300)
+        size = random.randint(5,25)
+        up()
+        goto(x,y)
+        down()
+        diamond(size)
+
+nightsky2()
+exitonclick()
 mainloop()
