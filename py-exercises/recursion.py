1ec991d92ca4d182cff3b0c47dd9a6cc1f8a1971
My Mac <chancecordelia@gmail.com>
2017-09-21 17:06:49 -0500

    recursion play

---
+++ py-exercises/recursion.py
@@ -0,0 +1,17 @@
+def factorialize(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * factorialize(n-1)
+
+# print(factorialize(4))
+
+
+def reverseList(lst):
+    if lst: #if list is not empty
+        return reverseList(lst[1:])+[lst[0]]
+    else:
+        return []
+
+test = [4,3,2,1]
+print(reverseList(test))
